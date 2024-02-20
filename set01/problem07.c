79b25d192cabc9049c6744f72325cf638b7c667a
Dhyeykumar Patel <dhyeykumark.bsc23@rvu.edu.in>
2024-02-20 23:39:13 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,3 +1,33 @@
-int input_n();
-int sum_n_nos(int n);
-void output(int n, int sum);
+#include <stdio.h>
+
+
+int input_n()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    return n;
+}
+
+int sum_n_nos(int n)
+{
+    int sum=0;
+    int i;
+    for(i=0;i<=n;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+void output(int n, int sum)
+{
+    printf("Sum of %d numbers is %d",n,sum);
+}
+int main()
+{
+    int n, sum;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0; 
+}
---
+++ set01/problem09.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
+
+int main()
+{
+    float n= input()
+    float sqrt=square_root(n);
+    output(n,sqrt);
+    return 0;
+}

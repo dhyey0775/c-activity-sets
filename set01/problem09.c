cab673d6553c493a1d4c667448173e316b96d43e
Dhyeykumar Patel <dhyeykumark.bsc23@rvu.edu.in>
2024-02-22 22:37:09 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -10,3 +10,28 @@ int main()
     output(n,sqrt);
     return 0;
 }
+
+float input()
+{
+    int n; 
+    printf("enter a number :");
+    scanf("%d" , &n);
+    return n;
+}
+
+float square_root(float n)
+{
+    float x_old=1 , y_new=n/2;
+    float xy = 0.000001;
+    while(fabs(y_new-x_old)>xy)
+    {
+        x_old=y_new;
+        y_new = 0.5*(x_old + n/x_old);
+    }
+    return y_new;
+}
+
+void output(float n, float sqrroot)
+{
+    print("The square root of %f is %f",n,sqrroot);
+}
